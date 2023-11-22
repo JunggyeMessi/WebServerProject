c379b82814728109d06e9407b7b13d7be5c67866
JunggyeMessi <herrymh@naver.com>
2023-11-22 14:27:04 +0900

    환경세팅 완료

---
+++ src/test/java/com/webserverproject/blog/WebserverprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webserverproject.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
